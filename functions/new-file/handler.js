0f51c481fd2474c3456eb2abf16e4647e7a702ee
Martín <chetinchog@yahoo.com>
2019-11-13 22:06:40 -0300

    Get  dimensions

--- functions/new-file/handler.js
+++ functions/new-file/handler.js
@@ -1,13 +1,7 @@
 "use strict";
 const MongoClient = require("mongodb").MongoClient;
 const url = "mongodb://func_mdb:27017/";
-
 const connect = MongoClient.connect(url, { useUnifiedTopology: true });
-const run = (conn, db, collection, foo, obj) =>
-  conn
-    .db(db)
-    .collection(collection)
-    [foo](obj);
 
 const splitKey = key => ({
   bucket: key.split("/")[0],
@@ -15,22 +9,33 @@ const splitKey = key => ({
   type: key.split("/")[1].split(".")[1]
 });
 
-module.exports = async (event, context) => {
+module.exports = async (req, res) => {
   try {
-    if (event.body.Key) {
-      const record = event.body.Records.find(
+    if (req.body.Key) {
+      const record = req.body.Records.find(
         record => record.eventName === "s3:ObjectCreated:Put"
       );
-      await run(await connect, "tp5", "file", "insertOne", {
-        _id: event.body.Key,
-        ...splitKey(event.body.Key),
-        date: record.eventTime,
-        size: record.s3.object.size
-      });
-      context.status(200).succeed("OK");
-    } else context.status(400).fail("No data");
+      (await connect)
+        .db("tp5")
+        .collection("file")
+        .update(
+          {
+            _id: req.body.Key
+          },
+          {
+            $set: {
+              _id: req.body.Key,
+              ...splitKey(req.body.Key),
+              date: record.eventTime,
+              size: record.s3.object.size
+            }
+          },
+          { upsert: true }
+        );
+      res.status(200).succeed("OK");
+    } else res.status(400).fail("No data");
   } catch (error) {
     console.log("TCL: error", error);
-    context.status(500).fail(error);
+    res.status(500).fail(error);
   }
 };
